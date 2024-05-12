4825f0a018ed308b0ee9f08b5d3c2d748b9ffe78
Sigurd Riseth <sigurd.riseth@gmail.com>
2024-05-12 14:57:18 +0200

    Implemented functionality to save a custom Julia Set

--- src/main/java/no/ntnu/idatg2003/controller/CreateCustomGameController.java
+++ src/main/java/no/ntnu/idatg2003/controller/CreateCustomGameController.java
@@ -1,6 +1,14 @@
 package no.ntnu.idatg2003.controller;
 
+import java.util.List;
 import javafx.scene.Scene;
+import no.ntnu.idatg2003.model.game.engine.ChaosGameDescription;
+import no.ntnu.idatg2003.model.game.engine.ChaosGameFileHandler;
+import no.ntnu.idatg2003.model.math.datatypes.Complex;
+import no.ntnu.idatg2003.model.math.datatypes.Vector2D;
+import no.ntnu.idatg2003.model.transformations.JuliaTransform;
+import no.ntnu.idatg2003.model.transformations.Transform2D;
+import no.ntnu.idatg2003.utility.LoggerUtil;
 import no.ntnu.idatg2003.view.ChaosGameApp;
 import no.ntnu.idatg2003.view.CreateCustomGame;
 
@@ -34,4 +42,21 @@ public class CreateCustomGameController {
     app.showMainScene();
   }
 
+  public void saveJuliaSet(String fileName) {
+    LoggerUtil.logInfo("Saving Julia set to file: " + fileName);
+    Vector2D min = createCustomGameView.getMinCoords();
+    Vector2D max = createCustomGameView.getMaxCoords();
+    Complex complexNumber = createCustomGameView.getComplexNumber();
+
+    List<Transform2D> transforms = List.of(new JuliaTransform(complexNumber, 1));
+
+    ChaosGameDescription chaosGameDescription = new ChaosGameDescription(min, max, transforms);
+
+    ChaosGameFileHandler.writeToFile(chaosGameDescription, fileName);
+    LoggerUtil.logInfo("Julia set saved");
+  }
+
+  public void saveAffineTransformation(String fileName) {
+    LoggerUtil.logInfo("Saving Affine transformation to file: " + fileName);
+  }
 }
--- src/main/java/no/ntnu/idatg2003/view/CreateCustomGame.java
+++ src/main/java/no/ntnu/idatg2003/view/CreateCustomGame.java
@@ -1,5 +1,7 @@
 package no.ntnu.idatg2003.view;
 
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -16,6 +18,8 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import no.ntnu.idatg2003.controller.CreateCustomGameController;
+import no.ntnu.idatg2003.model.math.datatypes.Complex;
+import no.ntnu.idatg2003.model.math.datatypes.Vector2D;
 
 /**
  * The CreateCustomGame class is a view class that creates the view for the Create Custom Game
@@ -34,7 +38,8 @@ import no.ntnu.idatg2003.controller.CreateCustomGameController;
 public class CreateCustomGame {
 
   private final CreateCustomGameController controller;
-
+  private GridPane juliaGrid;
+  private GridPane affineGrid;
 
   /**
    * Constructor for the CreateCustomGame class.
@@ -79,7 +84,7 @@ public class CreateCustomGame {
     vbox.setPadding(new Insets(10));
 
     // Julia Set Parameters
-    GridPane juliaGrid = new GridPane();
+    juliaGrid = new GridPane();
     juliaGrid.setVgap(5);
     juliaGrid.setHgap(5);
     addMinMaxToGridPane(juliaGrid);
@@ -88,7 +93,7 @@ public class CreateCustomGame {
     juliaGrid.setAlignment(javafx.geometry.Pos.CENTER);
 
     // Affine Transformation Parameters
-    GridPane affineGrid = new GridPane();
+    affineGrid = new GridPane();
     affineGrid.setVgap(5);
     affineGrid.setHgap(5);
     addMinMaxToGridPane(affineGrid);
@@ -99,10 +104,15 @@ public class CreateCustomGame {
     changeNodeColor(juliaGrid);
     changeNodeColor(affineGrid);
 
-    // Button to save parameters
-    HBox juliaBox = createSaveBox("Save Julia-Set");
+    // Limit user inputs to Doubles
+    limitInputsToDouble(juliaGrid);
+    limitInputsToDouble(affineGrid);
 
-    HBox affineBox = createSaveBox("Save Affine-Transformation");
+    // Button to save parameters
+    HBox juliaBox = createSaveBox("Save Julia-Set", controller::saveJuliaSet);
+
+    HBox affineBox = createSaveBox("Save Affine-Transformation",
+        controller::saveAffineTransformation);
 
     // Button to add a transformation
     Button addTransformationButton = new Button("Add Transformation");
@@ -127,17 +137,35 @@ public class CreateCustomGame {
     return content;
   }
 
+  private void limitInputsToDouble(GridPane grid) {
+    for (Node node : grid.getChildren()) {
+      if (node instanceof TextField textField) {
+        textFieldLimitToDouble(textField);
+      }
+    }
+  }
+
+  private static void textFieldLimitToDouble(TextField textField) {
+    textField.textProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.matches("-?\\d{0,7}([.]\\d{0,4})?")) {
+        textField.setText(oldValue);
+      }
+    });
+  }
+
   /**
    * Creates a save box with a text field and a button.
    *
    * @param saveText The text to display on the button.
+   * @param saveAction The action to perform when the button is clicked.
    * @return The save box.
    */
-  private static HBox createSaveBox(String saveText) {
+  private static HBox createSaveBox(String saveText, Consumer<String> saveAction) {
     TextField inputField = new TextField();
     inputField.setPromptText("Insert name");
     Button saveButton = new Button(saveText);
     HBox juliaBox = new HBox(10);
+    saveButton.setOnAction(e -> saveAction.accept(inputField.getText()));
 
     juliaBox.getChildren().addAll(inputField, saveButton);
     juliaBox.setAlignment(javafx.geometry.Pos.CENTER);
@@ -197,6 +225,9 @@ public class CreateCustomGame {
     xField.setMaxWidth(50);
     yField.setMaxWidth(50);
 
+    textFieldLimitToDouble(xField);
+    textFieldLimitToDouble(yField);
+
     VBox vbox = new VBox(5);
     vbox.getChildren().addAll(xField, yField);
     return vbox;
@@ -229,8 +260,52 @@ public class CreateCustomGame {
     TextField textField = new TextField();
     textField.setPromptText(promptText);
     textField.setMaxWidth(50);
+    textFieldLimitToDouble(textField);
     return textField;
   }
 
+  public Vector2D getMinCoords() {
+    AtomicReference<Double> x0 = new AtomicReference<>((double) 0);
+    AtomicReference<Double> x1 = new AtomicReference<>((double) 0);
+    juliaGrid.getChildren().forEach(node -> {
+      if (node instanceof TextField textField) {
+        if (GridPane.getRowIndex(node) == 0 && GridPane.getColumnIndex(node) == 1) {
+          x0.set(Double.parseDouble(textField.getText()));
+        } else if (GridPane.getRowIndex(node) == 1 && GridPane.getColumnIndex(node) == 1) {
+          x1.set(Double.parseDouble(textField.getText()));
+        }
+      }
+    });
+    return new Vector2D(x0.get(), x1.get());
+  }
 
+  public Vector2D getMaxCoords() {
+    AtomicReference<Double> x0 = new AtomicReference<>((double) 0);
+    AtomicReference<Double> x1 = new AtomicReference<>((double) 0);
+    juliaGrid.getChildren().forEach(node -> {
+      if (node instanceof TextField textField) {
+        if (GridPane.getRowIndex(node) == 2 && GridPane.getColumnIndex(node) == 1) {
+          x0.set(Double.parseDouble(textField.getText()));
+        } else if (GridPane.getRowIndex(node) == 3 && GridPane.getColumnIndex(node) == 1) {
+          x1.set(Double.parseDouble(textField.getText()));
+        }
+      }
+    });
+    return new Vector2D(x0.get(), x1.get());
+  }
+
+  public Complex getComplexNumber() {
+    AtomicReference<Double> real = new AtomicReference<>((double) 0);
+    AtomicReference<Double> imaginary = new AtomicReference<>((double) 0);
+    juliaGrid.getChildren().forEach(node -> {
+      if (node instanceof TextField textField) {
+        if (GridPane.getRowIndex(node) == 4 && GridPane.getColumnIndex(node) == 1) {
+          real.set(Double.parseDouble(textField.getText()));
+        } else if (GridPane.getRowIndex(node) == 5 && GridPane.getColumnIndex(node) == 1) {
+          imaginary.set(Double.parseDouble(textField.getText()));
+        }
+      }
+    });
+    return new Complex(real.get(), imaginary.get());
+  }
 }
